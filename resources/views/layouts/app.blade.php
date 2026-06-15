bc8d795c805b55be7076949b226939ca6c4207ea
ubayarashy <arashyubay@gmail.com>
2026-06-15 09:21:44 +0700

    Remove Vite, all CSS already inline

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     
     <!-- Vite Assets -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+ 
     
     <!-- Theme Script -->
     <script>
